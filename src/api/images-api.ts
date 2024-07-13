804936ce75219a03ba0716b193eda05ec160000b
Marianna Dei <d.marianna@icloud.com>
2024-07-13 11:26:11 +0300

    Added ts code to api

--- src/api/images-api.ts
+++ src/api/images-api.ts
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const ACCESS_KEY = `XwurAsxbIvpFOqYLfiOPooi-NtzQ2JO4IEDnUVnWzEE`;
 
 axios.defaults.baseURL = 'https://api.unsplash.com';
 
-export const getImagesApi = async (searchQuery, page) => {
-  const { data } = await axios.get('/search/photos', {
+export const getImagesApi = async (
+  searchQuery: string,
+  page: number
+): Promise<string[]> => {
+  const { data }: AxiosResponse = await axios.get('/search/photos', {
     params: {
       client_id: ACCESS_KEY,
       query: searchQuery,
